d8aa69b5df3b3b0e2499dd7f34c772f1e11f22be
zhouzhou <siri.O.potter@gmail.com>
2025-01-17 20:15:04 +0800

    init

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import '@/app/ui/global.css'
+import { inter, lusitana } from '@/app/ui/fonts';
+export default function RootLayout({
+  children,
+}:{children:React.ReactNode}){
+  return (
+    <html lang="en">
+      <body className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+        {children}
+      </body>
+    </html>
+  )
+}
